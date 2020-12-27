c88dc5e8552a1b20cfb13646313980628f9fccc6
mthsgrc <matheus.dev.garcia@gmail.com>
2020-12-27 19:23:45 -0300

    save/load game functional

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,16 +6,17 @@
   - redraw board with guess - all guesses
   - show wrong letters 
   - show left guesses
-  -  
+
+  - implement save/load game
 =end
 
 require "pry"
+require "yaml"
 require_relative "hangman_figures.rb"
 
 class Hangman
   attr_reader :secret_word
-  attr_accessor :guess, :wrong_letters_guessed, :rounds
-
+  attr_accessor :guess, :wrong_letters_guessed, :count_missed_letters
   TITLE = "====== HANGMAN GAME =======\n\n\n"
 
   def initialize
@@ -30,15 +31,60 @@ class Hangman
   def set_game(secret_word, guess)
     system("clear") || system("cls")
     print TITLE
-    @rounds = 0
-    p @secret_word
+    new_or_load_game
+  end
 
-    print "#{draw_hangman_figure(@rounds)}   "
+  def save_game
+    save = YAML.dump({
+                       :count_missed_letters => @count_missed_letters,
+                       :secret_word => @secret_word,
+                       :wrong_letters_guessed => @wrong_letters_guessed,
+                       :guess => @guess
+    })
+    File.open("saved.yaml", "w") do |file|
+      file.write save
+    end
+  end
 
+  def load_game(file)
+    state_loaded = YAML.load file
+
+    @count_missed_letters = state_loaded[:count_missed_letters]
+    @secret_word = state_loaded[:secret_word]
+    @wrong_letters_guessed = state_loaded[:wrong_letters_guessed]
+    @guess = state_loaded[:guess]
+
+    binding.pry
+    update_game(@guess)
+    get_letter_guess
+  end
+
+  def new_or_load_game
+    response = ""
+    print "Do you want to start a [N]ew Game or [L]oad a saved game?"
+    until response == "N" || response == "L"
+      response = gets.chomp.upcase
+    end
+
+    if response == "N"
+      new_game
+    elsif response == "L"
+      load_game(save_game_file)
+    end
+
+  end
+
+  def save_game_file
+  	File.open("./saved.yaml", "r")
+  end
+
+  def new_game
+    system("clear") || system("cls")
+    print TITLE
+    @count_missed_letters = 0
+    print "#{draw_hangman_figure(@count_missed_letters)}\t"
     create_guess_spaces
-
     print @guess
-
     puts "\n\nThe Secret Word has #{secret_word.length} letters.\n"
     get_letter_guess
   end
@@ -50,86 +96,70 @@ class Hangman
       char_spaces +=  "_ "
       count -= 1
     end
-
     @guess = char_spaces.strip
   end
 
   def get_letter_guess
     letter_guess = ""
-
-    # until letter_guess.length == 1 && letter_guess[0].match?(/[a-zçãáêéíóôú]/) || letter_guess == @secret_word
-    until letter_guess.length == 1 && letter_guess[0].match?(/[a-z]/) || letter_guess == @secret_word
+    # until letter_guess.length == 1 && letter_guess[0].match?(/[a-zçãáêéíóôú]/)
+    until letter_guess.length == 1 && letter_guess[0].match?(/[a-z]/) || letter_guess == "/save"
       puts "\nChoose a letter between 'a' and 'z':"
-      letter_guess = gets.chomp.downcase
+      puts "Write '/save' to save the game."
+      letter_guess = gets.chomp.downcase.strip
     end
-
-    test_letter(letter_guess)
+    letter_guess == "/save" ? save_game : test_letter(letter_guess)
   end
-
 
   def test_letter(letter)
     if @secret_word.include?(letter)
       puts "\n'#{letter}' included in Secret Word"
-      fill_letter(letter)
-
+      update_right_guess(letter)
     else
-
-      @rounds += 1
+      @count_missed_letters += 1
       puts "\n'#{letter}' is not included in Secret Word"
       @wrong_letters_guessed << letter
       @wrong_letters_guessed.uniq!
-
-
     end
-
     check_win
   end
 
-  def fill_letter(letter)
-
+  def update_right_guess(letter)
     array_secret = @secret_word.strip.split("")
     array_guess = @guess.gsub(" ", "").split("")
-
     array_secret.each_with_index do |char, idx|
       array_guess[idx] = letter if char == letter
     end
     @guess = array_guess.join
-
   end
 
 
   def update_game(new_guess)
+    system("clear") || system("cls")
+    print TITLE
     guess = new_guess.split("")
-
-    print "#{draw_hangman_figure(@rounds)}   "
+    print "#{draw_hangman_figure(@count_missed_letters)}   "
     guess.each { |char| print "#{char} " }
-
     puts "\n\nLetters wrongly guessed: #{@wrong_letters_guessed.join(" ").strip}\n\n"
   end
 
+
   def check_win
-    system("clear") || system("cls")
     if @secret_word == @guess
       print TITLE
       puts "Letters wrongly guessed: #{@wrong_letters_guessed.join(" ").strip}\n\n"
-
-      print draw_hangman_figure(@rounds)
+      print draw_hangman_figure(@count_missed_letters)
       puts "\tYou won! Secret word is #{@secret_word.upcase}.\n\n"
       exit
-    elsif @rounds == 7
-      print TITLE
+    elsif @count_missed_letters == 7
       update_game(@guess.gsub(" ", ""))
-
-      print draw_hangman_figure(@rounds-1)
+      print draw_hangman_figure(@count_missed_letters-1)
       puts "\tYou've lost! Secret word was #{@secret_word.upcase}.\n\n"
       exit
     else
-      print TITLE
       update_game(@guess.gsub(" ", ""))
-      puts "#{@rounds} wrong guesses. Make it 6 and you lose."
+      puts "#{@count_missed_letters} wrong guesses. Make it 6 and you lose."
     end
     get_letter_guess
-
   end
 
 
@@ -141,7 +171,5 @@ class Hangman
     word.chomp
   end
 end
-
-
 
 Hangman.new
--- lib/hangman_figures.rb
+++ lib/hangman_figures.rb
@@ -2,39 +2,42 @@
 def draw_hangman_figure(round)
   case round
   when 0  
-  r0 = "\t|---|\r
+  hangman = "\t|---|\r
         |    \r
         |      \r
         |      "
   when 1
-  r1 = "\t|---|\r
+  hangman = "\t|---|\r
         |   O\r
         |      \r
         |      "
   when 2
-  r2 = "\t|---|\r
+  hangman = "\t|---|\r
         |   O\r
         |  /   \r
         |      "
   when 3
-  r3 = "\t|---|\r
+  hangman = "\t|---|\r
         |   O\r
         |  / \\\r
         |      "
   when 4
-  r4 = "\t|---|\r
+  hangman = "\t|---|\r
         |   O\r
         |  /|\\\r
         |      "
   when 5
-  r5 = "\t|---|\r
+  hangman = "\t|---|\r
         |   O\r
         |  /|\\\r
         |  /   "
   when 6
-  r6 = "\t|---|\r
+  hangman = "\t|---|\r
         |   O\r
         |  /|\\\r
-        |  / \\ "
+        |  / \\\r 
+        |      "
  end
+ hangman
 end
+
